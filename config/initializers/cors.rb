b2bcc1769eae046eca48dcf3a958a42acacdd9b1
Alabi12 <wolo22001@gmail.com>
2023-05-17 22:42:36 +0000

    connect cor to the react application

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://192.168.255.185:3000' # Replace with the URL of your React frontend
-  
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-    end
+  allow do
+    origins ' http://localhost:3000' # Add the origin of your React frontend
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
+end
+
   
